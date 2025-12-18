03f84f6648855ded44574ea498a165236ddf3652
Satyajit Sahoo <satyajit28sahoo@gmail.com>
2025-12-19 00:21:59 +0530

    first commit

---
+++ src/test/java/com/credResolv/ExpenseSharingApplication/ExpenseSharingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.credResolv.ExpenseSharingApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseSharingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
